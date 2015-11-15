9decdc17432c82bae5695001aac21662df2c5411
Vasiliy Kirstia <kirstiavasilii@gmail.com>
2015-11-15 18:44:21 +0300

    немного переделал программу под использование из консоли

--- src/main.c
+++ src/main.c
@@ -15,103 +15,63 @@ void printResult(uint8_t *data,long long length){
 		printf("%x", (data[i] >> 4) );
 		printf("%x", data[i] & 0b1111);
 	};
-	puts("\n");
+	puts("");
 }
 
-typedef struct {
-	uint8_t *array;
-	long long int length;
-}ArrayOfByte;
+int main (int argc, char* argv[]){
+	int fd;
+	HashCodeLength hash_lengt;
 
-ArrayOfByte convertToArray(char *str, long long int length){
-	long long int a_len = (length >> 1) + (length & 1); //количество байтов требуемое для хранения результата.
-	uint8_t *buff = calloc(a_len, sizeof(uint8_t));
-
-	long long int j = 0;
-	for(long long int i = length - 1; i > -1; --i){
-		switch (str[i]) {
-			case '0':
-				buff[ j>>1 ] += 0U << ( (j & 1)*4 );
-				break;
-			case '1':
-				buff[ j>>1 ] += 1U << ( (j & 1)*4 );
-				break;
-			case '2':
-				buff[ j>>1 ] += 2U << ( (j & 1)*4 );
-				break;
-			case '3':
-				buff[ j>>1 ] += 3U << ( (j & 1)*4 );
-				break;
-			case '4':
-				buff[ j>>1 ] += 4U << ( (j & 1)*4 );
-				break;
-			case '5':
-				buff[ j>>1 ] += 5U << ( (j & 1)*4 );
-				break;
-			case '6':
-				buff[ j>>1 ] += 6U << ( (j & 1)*4 );
-				break;
-			case '7':
-				buff[ j>>1 ] += 7U << ( (j & 1)*4 );
-				break;
-			case '8':
-				buff[ j>>1 ] += 8U << ( (j & 1)*4 );
-				break;
-			case '9':
-				buff[ j>>1 ] += 9U << ( (j & 1)*4 );
-				break;
-			case 'a':
-			case 'A':
-				buff[ j>>1 ] += 10U << ( (j & 1)*4 );
-				break;
-			case 'b':
-			case 'B':
-				buff[ j>>1 ] += 11U << ( (j & 1)*4 );
-				break;
-			case 'c':
-			case 'C':
-				buff[ j>>1 ] += 12U << ( (j & 1)*4 );
-				break;
-			case 'd':
-			case 'D':
-				buff[ j>>1 ] += 13U << ( (j & 1)*4 );
-				break;
-			case 'e':
-			case 'E':
-				buff[ j>>1 ] += 14U << ( (j & 1)*4 );
-				break;
-			case 'f':
-			case 'F':
-				buff[ j>>1 ] += 15U << ( (j & 1)*4 );
-				break;
-			default:
-				puts("ERROR");
-				exit(1);
-				break;
+	if(argc > 1){
+		fd = open(argv[1], O_RDONLY);
+		if(-1 == fd){
+			puts("can`t open file");
+			return 1;
 		}
 
-		++j;
+		if(argc > 2){
+			if( 0 == strcmp(argv[2], "256") ){
+				hash_lengt = HASH_256;
+			}
+			else{
+				if( 0 == strcmp(argv[2], "512") ){
+					hash_lengt = HASH_512;
+				}
+				else{
+					puts("unknown hash length");
+					close(fd);
+					return 1;
+				}
+			}
+		}
+		else{
+			hash_lengt = HASH_512;
+		}
+	}
+	else{
+		puts("use: prog_name [file_for_hashing] [256|512]");
 	}
 
-	ArrayOfByte result;
-	result.array = buff;
-	result.length = a_len;
-
-	return result;
-}
-
-int main (void){
-	int fd = open("/tmp/f2", O_RDONLY);
 
 	struct stat fs;
 	fstat(fd, &fs);
 
 	void *data = mmap(NULL, fs.st_size, PROT_READ, MAP_SHARED, fd, 0);
 
-	uint8_t *dest[64];
 
-	Encrypt(HASH_512, data, fs.st_size * 8, dest);
-	printResult(dest, 64);
+
+	if(hash_lengt == HASH_256){
+		uint8_t *dest[32];
+		Encrypt(hash_lengt, data, fs.st_size * 8, dest);
+		printResult(dest, 32);
+	}
+	else{
+		if( hash_lengt == HASH_512 ){
+			uint8_t *dest[64];
+			Encrypt(hash_lengt, data, fs.st_size * 8, dest);
+			printResult(dest, 64);
+		}
+	}
 	return 0;
 }
 
